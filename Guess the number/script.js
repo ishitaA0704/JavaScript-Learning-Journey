aaa7586a4bb2b0075abe0b1419cc9555ea95dc68
ishitaA0704 <ishitaagarwal3004@gmail.com>
2026-05-04 16:18:21 +0530

    js added

---
+++ Guess the number/script.js
@@ -0,0 +1,30 @@
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess;
+let run = true;
+
+while (run) {
+  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+  guess = Number(guess);
+
+  if (isNaN(guess)) {
+    window.alert("Please enter a valid number");
+  } else if (guess < minNum || guess > maxNum) {
+    window.alert("Please enter a valid number");
+  } else {
+    attempts++;
+    if (guess < answer)
+         {
+      window.alert("TOO LOW! TRY AGAIN!");
+    } else if (guess > answer)
+         {  
+      window.alert("TOO HIGH! TRY AGAIN!");
+    } else {
+      window.alert(`YOU GOT IT! The answer was ${answer}. Attempts made: ${attempts}`);
+      run = false;
+    }
+  }
+}
