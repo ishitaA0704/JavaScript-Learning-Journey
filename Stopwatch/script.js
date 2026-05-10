4cedf6bd3f2c950af337da763aeba9f798bfc8a4
ishitaA0704 <ishitaagarwal3004@gmail.com>
2026-05-10 18:51:55 +0530

    Changes

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,8 +1,8 @@
 const display = document.getElementById("display");
 
 let timer = null;
-let start = 0;
-let elapsed= 0;
+let startTime = 0;
+let elapsedTime = 0;
 let running = false;
 
 function start() {
@@ -10,33 +10,40 @@ function start() {
     if (running) return;
 
     running = true;
-    start = Date.now() - elapsed;
+    startTime = Date.now() - elapsedTime;
+
     timer = setInterval(update, 10);
 }
 
 function stop() {
 
     if (!running) return;
+
     running = false;
     clearInterval(timer);
-    elapsed = Date.now() - start;
+
+    elapsedTime = Date.now() - startTime;
 }
 
 function reset() {
 
     clearInterval(timer);
+
     running = false;
-    start = 0;
-    elapsed = 0;
+    startTime = 0;
+    elapsedTime = 0;
+
     display.textContent = "00:00:00:00";
 }
 
 function update() {
-    elapsed = Date.now() - start;
-    let h = Math.floor(elapsed / 3600000);
-    let m = Math.floor(elapsed / 60000) % 60;
-    let s = Math.floor(elapsed / 1000) % 60;
-    let ms = Math.floor(elapsed % 1000 / 10);
+
+    elapsedTime = Date.now() - startTime;
+
+    let h = Math.floor(elapsedTime / 3600000);
+    let m = Math.floor(elapsedTime / 60000) % 60;
+    let s = Math.floor(elapsedTime / 1000) % 60;
+    let ms = Math.floor(elapsedTime % 1000 / 10);
 
     display.textContent =
         `${String(h).padStart(2, "0")}:` +
